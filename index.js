45b8ca16d5b46d663ae0674eb02675f7ce431b92
co-den <iamagugbueikenna@gmail.com>
2025-04-27 16:09:36 +0100

    Fix bundling issues

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var $lj2gY$axios = require("axios");
+var $eOCCT$axios = require("axios");
 
 
 function $parcel$interopDefault(a) {
   return a && a.__esModule ? a.default : a;
 }
-/* eslint-disable */ /* eslint-disable */ const $da1b53875274984e$export$4c5dd147b21b9176 = (locations)=>{
+/* eslint-disable */ /* eslint-disable */ const $da7c3a1cbc8b150a$export$4c5dd147b21b9176 = (locations)=>{
     mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A';
     var map = new mapboxgl.Map({
         container: 'map',
@@ -41,23 +41,23 @@ function $parcel$interopDefault(a) {
 
 
 /* eslint-disable */ 
-/* eslint-disable */ const $5a20143dba361070$export$516836c6a9dfc573 = ()=>{
+/* eslint-disable */ const $2aa7e4f71e700ad4$export$516836c6a9dfc573 = ()=>{
     const el = document.querySelector('.alert');
     if (el) el.parentElement.removeChild(el);
     console.log('Alert hidden');
 };
-const $5a20143dba361070$export$de026b00723010c1 = (type, msg, time = 7)=>{
-    $5a20143dba361070$export$516836c6a9dfc573();
+const $2aa7e4f71e700ad4$export$de026b00723010c1 = (type, msg, time = 7)=>{
+    $2aa7e4f71e700ad4$export$516836c6a9dfc573();
     const markup = `<div class="alert alert--${type}">${msg}</div>`;
     document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
     console.log(`Alert shown: ${type} - ${msg}`);
-    window.setTimeout($5a20143dba361070$export$516836c6a9dfc573, time * 1000);
+    window.setTimeout($2aa7e4f71e700ad4$export$516836c6a9dfc573, time * 1000);
 };
 
 
-const $bda1f8f48a2d604b$export$596d806903d1f59e = async (email, password)=>{
+const $7e2e6df33960c717$export$596d806903d1f59e = async (email, password)=>{
     try {
-        const res = await (0, ($parcel$interopDefault($lj2gY$axios)))({
+        const res = await (0, ($parcel$interopDefault($eOCCT$axios)))({
             method: "POST",
             url: "http://127.0.0.1:3000/api/v1/users/login",
             data: {
@@ -67,7 +67,7 @@ const $bda1f8f48a2d604b$export$596d806903d1f59e = async (email, password)=>{
             }
         });
         if (res.data.status === "success") {
-            (0, $5a20143dba361070$export$de026b00723010c1)("success", "Logged in successfully!");
+            (0, $2aa7e4f71e700ad4$export$de026b00723010c1)("success", "Logged in successfully!");
             window.setTimeout(()=>{
                 console.log("Redirecting to home page...");
                 location.assign("/");
@@ -76,12 +76,12 @@ const $bda1f8f48a2d604b$export$596d806903d1f59e = async (email, password)=>{
     } catch (err) {
         console.log("Login failed for email:", email);
         console.log("Error details:", err.response); // Log the full error response
-        (0, $5a20143dba361070$export$de026b00723010c1)("error", err.response.data.message);
+        (0, $2aa7e4f71e700ad4$export$de026b00723010c1)("error", err.response.data.message);
     }
 };
-const $bda1f8f48a2d604b$export$7200a869094fec36 = async (name, email, password, passwordConfirm)=>{
+const $7e2e6df33960c717$export$7200a869094fec36 = async (name, email, password, passwordConfirm)=>{
     try {
-        const res = await (0, ($parcel$interopDefault($lj2gY$axios)))({
+        const res = await (0, ($parcel$interopDefault($eOCCT$axios)))({
             method: "POST",
             url: "http://127.0.0.1:3000/api/v1/users/signup",
             data: {
@@ -92,7 +92,7 @@ const $bda1f8f48a2d604b$export$7200a869094fec36 = async (name, email, password, 
             }
         });
         if (res.data.status === "success") {
-            (0, $5a20143dba361070$export$de026b00723010c1)("success", "Signed up successfully!");
+            (0, $2aa7e4f71e700ad4$export$de026b00723010c1)("success", "Signed up successfully!");
             window.setTimeout(()=>{
                 console.log("Redirecting to home page...");
                 location.assign("/");
@@ -101,100 +101,100 @@ const $bda1f8f48a2d604b$export$7200a869094fec36 = async (name, email, password, 
     } catch (err) {
         console.log("Signup failed for email:", email);
         console.log("Error details:", err.response); // Log the full error response
-        (0, $5a20143dba361070$export$de026b00723010c1)("error", err.response.data.message);
+        (0, $2aa7e4f71e700ad4$export$de026b00723010c1)("error", err.response.data.message);
     }
 };
-const $bda1f8f48a2d604b$export$a0973bcfe11b05c9 = async ()=>{
+const $7e2e6df33960c717$export$a0973bcfe11b05c9 = async ()=>{
     try {
-        const res = await (0, ($parcel$interopDefault($lj2gY$axios)))({
+        const res = await (0, ($parcel$interopDefault($eOCCT$axios)))({
             method: "GET",
             url: "http://127.0.0.1:3000/api/v1/users/logout"
         });
         res.data.status = "success";
         console.log("Logout successful");
-        (0, $5a20143dba361070$export$de026b00723010c1)("success", "Logout successful");
+        (0, $2aa7e4f71e700ad4$export$de026b00723010c1)("success", "Logout successful");
         location.reload(true);
     } catch (err) {
         console.log("Logout failed");
-        (0, $5a20143dba361070$export$de026b00723010c1)("error", "Error logging out! Try again.");
+        (0, $2aa7e4f71e700ad4$export$de026b00723010c1)("error", "Error logging out! Try again.");
     }
 };
 
 
 /* eslint-disable */ 
 
-const $e90ec163d41d80eb$export$f558026a994b6051 = async (data, type)=>{
+const $ad27d9687e6e2901$export$f558026a994b6051 = async (data, type)=>{
     try {
         const url = type === 'password' ? '/api/v1/users/updateMyPassword' : '/api/v1/users/updateMe';
         console.log(`Updating ${type} settings`);
-        const res = await (0, ($parcel$interopDefault($lj2gY$axios)))({
+        const res = await (0, ($parcel$interopDefault($eOCCT$axios)))({
             method: 'PATCH',
             url: url,
             data: data
         });
         if (res.data.status === 'success') {
             console.log(`${type.toUpperCase()} updated successfully`);
-            (0, $5a20143dba361070$export$de026b00723010c1)('success', `${type.toUpperCase()} updated successfully!`);
+            (0, $2aa7e4f71e700ad4$export$de026b00723010c1)('success', `${type.toUpperCase()} updated successfully!`);
         }
     } catch (err) {
         console.log(`Error updating ${type} settings:`, err.response.data.message);
-        (0, $5a20143dba361070$export$de026b00723010c1)('error', err.response.data.message);
+        (0, $2aa7e4f71e700ad4$export$de026b00723010c1)('error', err.response.data.message);
     }
 };
 
 
 
 // DOM ELEMENTS
-const $bfaeeca4df13dc1c$var$mapBox = document.getElementById("map");
-const $bfaeeca4df13dc1c$var$signupForm = document.querySelector(".form--signup");
-const $bfaeeca4df13dc1c$var$loginForm = document.querySelector(".form--login");
-const $bfaeeca4df13dc1c$var$logOutBtn = document.querySelector(".nav__el--logout");
-const $bfaeeca4df13dc1c$var$userDataForm = document.querySelector(".form-user-data");
-const $bfaeeca4df13dc1c$var$userPasswordForm = document.querySelector(".form-user-password");
-const $bfaeeca4df13dc1c$var$bookBtn = document.getElementById("book-tour");
+const $443a3c75b7aa6a72$var$mapBox = document.getElementById("map");
+const $443a3c75b7aa6a72$var$signupForm = document.querySelector(".form--signup");
+const $443a3c75b7aa6a72$var$loginForm = document.querySelector(".form--login");
+const $443a3c75b7aa6a72$var$logOutBtn = document.querySelector(".nav__el--logout");
+const $443a3c75b7aa6a72$var$userDataForm = document.querySelector(".form-user-data");
+const $443a3c75b7aa6a72$var$userPasswordForm = document.querySelector(".form-user-password");
+const $443a3c75b7aa6a72$var$bookBtn = document.getElementById("book-tour");
 // DELEGATION
-if ($bfaeeca4df13dc1c$var$mapBox) {
-    const locations = JSON.parse($bfaeeca4df13dc1c$var$mapBox.dataset.locations);
+if ($443a3c75b7aa6a72$var$mapBox) {
+    const locations = JSON.parse($443a3c75b7aa6a72$var$mapBox.dataset.locations);
     console.log("Displaying map with locations:", locations);
-    (0, $da1b53875274984e$export$4c5dd147b21b9176)(locations);
+    (0, $da7c3a1cbc8b150a$export$4c5dd147b21b9176)(locations);
 }
-if ($bfaeeca4df13dc1c$var$signupForm) $bfaeeca4df13dc1c$var$signupForm.addEventListener("submit", (e)=>{
+if ($443a3c75b7aa6a72$var$signupForm) $443a3c75b7aa6a72$var$signupForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const passwordConfirm = document.getElementById("password-confirm").value;
     console.log("Signing up with email:", email);
-    (0, $bda1f8f48a2d604b$export$7200a869094fec36)(name, email, password, passwordConfirm);
+    (0, $7e2e6df33960c717$export$7200a869094fec36)(name, email, password, passwordConfirm);
 });
-if ($bfaeeca4df13dc1c$var$loginForm) $bfaeeca4df13dc1c$var$loginForm.addEventListener("submit", (e)=>{
+if ($443a3c75b7aa6a72$var$loginForm) $443a3c75b7aa6a72$var$loginForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     console.log("Logging in with email:", email);
-    (0, $bda1f8f48a2d604b$export$596d806903d1f59e)(email, password);
+    (0, $7e2e6df33960c717$export$596d806903d1f59e)(email, password);
 });
-if ($bfaeeca4df13dc1c$var$logOutBtn) $bfaeeca4df13dc1c$var$logOutBtn.addEventListener("click", ()=>{
+if ($443a3c75b7aa6a72$var$logOutBtn) $443a3c75b7aa6a72$var$logOutBtn.addEventListener("click", ()=>{
     console.log("Logging out user");
-    (0, $bda1f8f48a2d604b$export$a0973bcfe11b05c9)();
+    (0, $7e2e6df33960c717$export$a0973bcfe11b05c9)();
 });
-if ($bfaeeca4df13dc1c$var$userDataForm) $bfaeeca4df13dc1c$var$userDataForm.addEventListener("submit", (e)=>{
+if ($443a3c75b7aa6a72$var$userDataForm) $443a3c75b7aa6a72$var$userDataForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const form = new FormData();
     form.append("name", document.getElementById("name").value);
     form.append("email", document.getElementById("email").value);
     form.append("photo", document.getElementById("photo").files[0]);
     console.log("Updating user data");
-    (0, $e90ec163d41d80eb$export$f558026a994b6051)(form, "data");
+    (0, $ad27d9687e6e2901$export$f558026a994b6051)(form, "data");
 });
-if ($bfaeeca4df13dc1c$var$userPasswordForm) $bfaeeca4df13dc1c$var$userPasswordForm.addEventListener("submit", async (e)=>{
+if ($443a3c75b7aa6a72$var$userPasswordForm) $443a3c75b7aa6a72$var$userPasswordForm.addEventListener("submit", async (e)=>{
     e.preventDefault();
     document.querySelector(".btn--save-password").textContent = "Updating...";
     const passwordCurrent = document.getElementById("password-current").value;
     const password = document.getElementById("password").value;
     const passwordConfirm = document.getElementById("password-confirm").value;
     console.log("Updating user password");
-    await (0, $e90ec163d41d80eb$export$f558026a994b6051)({
+    await (0, $ad27d9687e6e2901$export$f558026a994b6051)({
         passwordCurrent: passwordCurrent,
         password: password,
         passwordConfirm: passwordConfirm
@@ -204,10 +204,10 @@ if ($bfaeeca4df13dc1c$var$userPasswordForm) $bfaeeca4df13dc1c$var$userPasswordFo
     document.getElementById("password").value = "";
     document.getElementById("password-confirm").value = "";
 });
-const $bfaeeca4df13dc1c$var$alertMessage = document.querySelector("body").dataset.alert;
-if ($bfaeeca4df13dc1c$var$alertMessage) {
-    console.log("Showing alert message:", $bfaeeca4df13dc1c$var$alertMessage);
-    (0, $5a20143dba361070$export$de026b00723010c1)("success", $bfaeeca4df13dc1c$var$alertMessage, 20);
+const $443a3c75b7aa6a72$var$alertMessage = document.querySelector("body").dataset.alert;
+if ($443a3c75b7aa6a72$var$alertMessage) {
+    console.log("Showing alert message:", $443a3c75b7aa6a72$var$alertMessage);
+    (0, $2aa7e4f71e700ad4$export$de026b00723010c1)("success", $443a3c75b7aa6a72$var$alertMessage, 20);
 }
 
 
